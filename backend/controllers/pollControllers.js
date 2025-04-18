4c9555d45bedd59615f4c32be92af78d37efb52a
Rohitkumar367 <rohitddroid@gmail.com>
2025-04-18 15:30:29 +0530

    getAllPolls api done

--- backend/controllers/pollControllers.js
+++ backend/controllers/pollControllers.js
@@ -87,7 +87,70 @@ export const getAllPolls = async (req, res) => {
     if(creatorId) filter.creator=creatorId;
 
     try {
+        const pageNumber = parseInt(page, 10);// string to number using base 10 -> into decimal number
+        const pageSize = parseInt(limit, 10)
+        const skip = (pageNumber - 1) * pageSize;
 
+        // fetching polls with pagination
+        const polls = await PollModel.find(filter).populate("creator", "fullName username email profileImageUrl").populate({
+            path: "responses.voterId",
+            select: "username profileImageUrl fullName",
+        }).skip(skip).limit(pageSize).sort({createdAt: -1});
+
+        // adding "userHasVoted" flag for each poll
+        const updatedPolls = polls.map((poll)=>{
+            const userHasVoted = poll.voters.some((voterId)=>voterId.equals(userId)); //.some() returns true if any of the elements in the array pass the test implemented by the provided function
+            return {
+                ...poll.toObject(), 
+                userHasVoted
+            };
+        })
+
+        // get total count of polls for pagination metadata
+        const totalPolls = await PollModel.countDocuments(filter);
+
+        // this aggregate is running on all data, not just the filtered data
+        const stats = await PollModel.aggregate([
+            {
+                $group:{
+                    _id: "$type",
+                    count: {$sum: 1},
+                }
+            },
+            {
+                $project:{
+                    type: "$_id",
+                    count: 1,
+                    _id: 0,
+                }
+            }
+        ]); // but his will not include the zero counts poll type
+
+        // ensure all types are included in stats, even those with zero counts
+        const allTypes=[
+            {type: "single-choice", label: "Single Choice"},
+            {type: "rating", label: "Rating"},
+            {type: "yes/no", label: "Yes/No"},
+            {type: "image-based", label: "Image Based"},
+            {type: "open-ended", label: "Open Ended"},
+        ];
+
+        const statsWithDefaults = allTypes.map((pollType)=>{
+            const newstat = stats.find((item)=> item.type === pollType.type);
+            return {
+                label: pollType.label,
+                type: pollType.type,
+                count: newstat ? newstat.count : 0,
+            }
+        }).sort((a,b) => b.count-a.count);
+
+        res.status(200).json({
+            polls: updatedPolls, // updated polls, i.e., userHasVoted on that poll or not
+            currentPage: pageNumber,
+            totalPages: Math.ceil(totalPolls/pageSize),
+            totalPolls,
+            stats: statsWithDefaults, // updated stats
+        });
     }catch(err){
         res.status(500).json({message:"Error getting all polls", error: err.message})
     }
